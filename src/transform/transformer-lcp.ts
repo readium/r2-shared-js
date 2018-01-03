a36ba87c4bbb28dacba883494f5b76ed02d22ff4
danielweck <daniel.weck@gmail.com>
2018-01-03 22:33:43 +0000

    LCP transform supports() function externalized

--- src/transform/transformer-lcp.ts
+++ src/transform/transformer-lcp.ts
@@ -1,41 +1,18 @@
 import { Publication } from "@models/publication";
 import { Link } from "@models/publication-link";
-import { transformStream as transformStream_ } from "@r2-lcp-js/transform/transformer-lcp";
+import { supports as supports_, transformStream as transformStream_ } from "@r2-lcp-js/transform/transformer-lcp";
 import { IStreamAndLength } from "@utils/zip/zip";
-import * as debug_ from "debug";
 
 import { ITransformer } from "./transformer";
 
-const debug = debug_("r2:transformer:lcp");
+// import * as debug_ from "debug";
+// const debug = debug_("r2:transformer:lcp");
 // const debugx = debug_("r2:transformer:stream:lcp");
 
 export class TransformerLCP implements ITransformer {
 
     public supports(publication: Publication, link: Link): boolean {
-
-        if (!publication.LCP) {
-            return false;
-        }
-
-        if (!publication.LCP.isReady()) {
-            debug("LCP not ready!");
-            return false;
-        }
-
-        const check = link.Properties.Encrypted.Scheme === "http://readium.org/2014/01/lcp"
-            && (link.Properties.Encrypted.Profile === "http://readium.org/lcp/basic-profile" ||
-                link.Properties.Encrypted.Profile === "http://readium.org/lcp/profile-1.0")
-            && link.Properties.Encrypted.Algorithm === "http://www.w3.org/2001/04/xmlenc#aes256-cbc"
-            ;
-        if (!check) {
-            debug("Incorrect resource LCP fields.");
-            debug(link.Properties.Encrypted.Scheme);
-            debug(link.Properties.Encrypted.Profile);
-            debug(link.Properties.Encrypted.Algorithm);
-            return false;
-        }
-
-        return true;
+        return supports_(publication.LCP, link.Href, link.Properties.Encrypted);
     }
 
     public async transformStream(
