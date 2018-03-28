842be6ff406c782a6cf786c611024e4d610c2299
danielweck <daniel.weck@gmail.com>
2018-03-28 16:59:28 +0100

    EDRLab to Readium migration

--- tools/gitrev.js
+++ tools/gitrev.js
@@ -31,7 +31,7 @@ try {
     if (branch.indexOf(detached) === 0) {
         branch = branch.substr(detached.length);
     }
-    const gitUrlBase = "https://github.com/edrlab/r2-shared-js/";
+    const gitUrlBase = "https://github.com/readium/r2-shared-js/";
     json = {
         "short": git.short(),
         "long": git.long(),
