f83c43923d4d24aa8f80551bc29a5edb081c7310
Daniel Weck <daniel.weck@gmail.com>
2019-04-10 14:24:18 +0100

    added JSON Schema references to Locator

--- src/models/locator.ts
+++ src/models/locator.ts
@@ -6,21 +6,56 @@
 // ==LICENSE-END==
 
 export interface Locator {
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L7
     href: string;
+
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L16
     title?: string;
+
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L39
     text?: LocatorText;
+
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L20
     locations: LocatorLocations;
+
+    // TODO
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L12
+    // type: string;
 }
 
 export interface LocatorText {
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L42
     before?: string;
+
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L46
     highlight?: string;
+
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L50
     after?: string;
 }
 
 export interface LocatorLocations {
+
+    // TODO
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L23
+    // fragment?: string;
     cfi?: string;
     cssSelector?: string;
+
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L32
     position?: number;
+
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/architecture/blob/f90b440dc3aa63c59981e3e46a7db7b8a545c613/schema/locator.schema.json#L27
     progression?: number;
 }
